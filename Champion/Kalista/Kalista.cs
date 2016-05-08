3263ac4a0a282930ad5dc44589e3f5cc6e1fa954
berbb <tintinridge@gmail.com>
2016-05-08 13:44:50 -0500

    Fix Kalista E & Damage %

--- Champion/Kalista/Kalista.cs
+++ Champion/Kalista/Kalista.cs
@@ -186,17 +186,9 @@ namespace iKalistaReborn
             {
                 foreach (var source in HeroManager.Enemies.Where(x => ObjectManager.Player.Distance(x) <= 2000f && !x.IsDead && x.IsHPBarRendered))
                 {
-                    var currentPercentage = Math.Round(Helper.GetRendDamage(source) * 100 / source.GetHealthWithShield(), 2);
+                    var currentPercentage = Math.Round(Helper.GetRendDamage(source) * 100 / source.GetHealthWithShield(), 1);
 
-                    Drawing.DrawText(
-                        Drawing.WorldToScreen(source.Position)[0],
-                        Drawing.WorldToScreen(source.Position)[1],
-                        currentPercentage >= 100
-                            ? Color.DarkOliveGreen
-                            : Color.White,
-                        currentPercentage >= 100
-                            ? "Killable With E"
-                            : "Current Damage: " + currentPercentage + "%");
+                    Drawing.DrawText(Drawing.WorldToScreen(source.Position)[0], Drawing.WorldToScreen(source.Position)[1], currentPercentage >= 100 ? Color.DarkOliveGreen : Color.White, currentPercentage >= 100 ? "Killable With E" : "Current Damage: " + Math.Ceiling(Helper.GetRendDamage(source) * 100 / source.GetHealthWithShield()) + "%");
                 }
             }
         }
