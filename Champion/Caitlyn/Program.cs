5ab8c1d018b19f82972aee9158fff83456d86693
berbb <tintinridge@gmail.com>
2016-05-10 15:35:01 -0500

    Trap Rework - OKTW

--- Champion/Caitlyn/Program.cs
+++ Champion/Caitlyn/Program.cs
@@ -276,14 +276,11 @@ namespace PortAIO.Champion.Caitlyn
                 }
 
                 if (getCheckBoxItem(wMenu, "telE"))
-                    foreach (
-                        var Object in
-                            ObjectManager.Get<Obj_AI_Base>()
-                                .Where(
-                                    Obj =>
-                                        Obj.Team != Player.Team && Obj.Distance(Player.ServerPosition) < W.Range &&
-                                        (Obj.HasBuff("teleport_target") || Obj.HasBuff("Pantheon_GrandSkyfall_Jump"))))
-                        W.Cast(Object.Position, true);
+                {
+                    var trapPos = OktwCommon.GetTrapPos(W.Range);
+                    if (!trapPos.IsZero)
+                        W.Cast(trapPos);
+                }
             }
         }
 
